a35e4cdeb0d73fa4ee36276ff66702c55c904af2
Xdeon <hongyfeng123@gmail.com>
2017-07-25 15:12:27 -0600

    Update simple test

--- test/ecoap_bench_test.erl
+++ test/ecoap_bench_test.erl
@@ -16,9 +16,11 @@ througput_test_() ->
 	}.
 
 run_throughput(_State) ->
-	Result = ecoap_bench_server:start_test(10, 3, "coap://127.0.0.1/benchmark"),
 	[
-	?_assertMatch(#{time:=_, sent:=_, rec:=_, timeout:=_, 
-					throughput:=_, min:=_, max:=_, mean:=_, 
-					median:=_, stddev:=_, ptile95:=_}, Result)
+	?_assertMatch(#{time:=Time, sent:=Sent, rec:=Rec, timeout:=Timeout, throughput:=Throughput, 
+					min:=Min, max:=Max, mean:=Mean, median:=Median, stddev:=Stddev, ptile95:=P95} 
+					when is_float(Time) and is_integer(Sent) and is_integer(Rec) and is_integer(Timeout) 
+						and is_float(Throughput) and is_float(Min) and is_float(Max) and is_float(Mean) 
+						and is_float(Median) and is_float(Stddev) and is_float(P95),
+					ecoap_bench_server:start_test(20, 3, "coap://127.0.0.1/benchmark"))
 	].
